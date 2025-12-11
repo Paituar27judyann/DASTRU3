a7d5f2dab9c6bf34d779181e721f1e04d8ce4cc8
Paituar27judyann <ann27.paituar@gmail.com>
2025-12-11 13:51:51 +0800

    Add files via upload

---
+++ OOP_Review2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OOP_Review2.Shape;
+
+namespace OOP_Review2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Circle object
+            Circle circle = new Circle();
+            circle.Color = "Red";
+            circle.Radius = 20;
+
+            Console.WriteLine("Shape: Circle");
+            Console.WriteLine("Shapes color: "+ circle.Color );
+            Console.WriteLine("Radius: " + circle.Radius );
+            Console.WriteLine("GetArea: " + circle.GetArea().ToString("F2"));
+
+          
+
+            // Rectangle object
+            Rectangle rect = new Rectangle();
+            rect.Color = "Blue";
+            rect.Width = 20;
+            rect.Height = 10;
+
+
+            Console.WriteLine("\nShape: Rectangle");
+            Console.WriteLine("Shape color: " + rect.Color );
+            Console.WriteLine("Dimention: " + rect.Width + " x " + rect.Height );
+            Console.WriteLine("Area: " + rect.GetArea().ToString("F2") );
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_Review2/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Review2
+{
+    public class Shape
+    {
+        //Base Class
+        public class shape
+        {
+            public string Color { get; set; }
+            public virtual double GetArea()
+            { return 0.0; }
+            
+        }
+        //Derived Class
+        public class Circle : shape
+        {
+            public double Radius { get; set; }
+            public override double GetArea()
+            {
+                return Math.PI * Math.Pow(Radius, 2);
+            }
+        }
+        //Drived class
+        public class Rectangle : shape
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public override double GetArea()
+            {
+                return Height * Width;
+            }
+        }
+    }
+}
