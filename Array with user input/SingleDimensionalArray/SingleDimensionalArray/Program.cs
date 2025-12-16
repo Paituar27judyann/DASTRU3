2eb1f053c216c61337f915ed46f88044ad5af913
Paituar27judyann <ann27.paituar@gmail.com>
2025-12-16 14:04:40 +0800

    Add files via upload

---
+++ Array with user input/SingleDimensionalArray/SingleDimensionalArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Judy Ann Paituar
+            //Problem:ACTIVITY 1 Single Dimensional Array in Procedural Programming
+
+            //declare an array 
+            string[] countries = new string[5];
+            
+            //Use loop to get user input
+            for(int asia =0; asia < countries.Length; asia++)
+            {
+                Console.Write("\nEnter Country in Asia: ", asia + 1);
+                countries[asia] = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(countries[asia]))
+                {
+                    Console.WriteLine("\tInvlid Input!");
+                    asia--;
+                }
+            }
+            //sorting your output alphabetically
+            Array.Sort(countries);
+            Console.WriteLine("\nThe Countries in Asia that you entered are:");
+
+            //Display Input
+            foreach(string country in countries)
+            {
+                Console.WriteLine(country);
+            }
+            Console.ReadKey();
+        }
+    }
+}
