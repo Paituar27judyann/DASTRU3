5d87c722ddedc24a5648b98c6220eede8f3abc39
Paituar27judyann <ann27.paituar@gmail.com>
2026-01-08 14:03:06 +0800

    Add files via upload

---
+++ Paituar_Single Dimensional Array of Objects in OOP/Country.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paituar_Single_Dimensional_Array_of_Objects_in_OOP
+{
+    internal class Country
+    {
+        // Countries Properties
+        public string Continent { get; set; }
+        public string Language { get; set; }
+
+        public Country(string continent, string language)
+        {
+            Continent = continent;
+            Language = language;
+        }
+
+        public virtual void DisplayCountriesInfo()
+        {
+            Console.WriteLine("Continent: " + Continent);
+            Console.WriteLine("Language: " + Language);
+        }
+    }
+
+    class Philippines : Country
+    {
+        public string NationalHero { get; set; }
+        public Philippines(string continent, string language, string nationalHero)
+            : base(continent, language)
+        {
+            NationalHero = nationalHero;
+        }
+
+        public override void DisplayCountriesInfo()
+        {
+            Console.WriteLine("\n\t\t\t\t--- Countries Information ---");
+            Console.WriteLine("\t\t\t\tContinent: " + Continent);
+            Console.WriteLine("\t\t\t\tLanguage: " + Language);
+            Console.WriteLine("\t\t\t\tNational Hero: " + NationalHero);
+
+        }
+    }
+}
---
+++ Paituar_Single Dimensional Array of Objects in OOP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paituar_Single_Dimensional_Array_of_Objects_in_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Name: Judy Ann Paituar
+            // Problem: ACTIVITY 2: Single Dimensional Array of Objects in OOP
+
+            Console.Write("Enter how many countries you want to input: ");
+            if (!int.TryParse(Console.ReadLine(), out int size) || size <= 0)
+            {
+                Console.WriteLine("Invalid input. Defaulting to 1.");
+                size = 1;
+            }
+
+            Country[] countries = new Country[size];
+
+            // Loop
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"\nEnter details for Country #{i + 1}:");
+
+                Console.Write("Continent: ");
+                string cont = Console.ReadLine();
+
+                Console.Write("Language: ");
+                string lang = Console.ReadLine();
+
+                Console.Write("National Hero: ");
+                string hero = Console.ReadLine();
+
+
+                countries[i] = new Philippines(cont, lang, hero);
+            }
+
+            foreach (Country country in countries)
+            {
+                country.DisplayCountriesInfo();
+            }
+
+           
+            Console.ReadKey();
+        }
+
+    }
+    
+}
